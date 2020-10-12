128bc512a3ab447c46899d92b96be5cba18277a7
Breens <breensthomas7353@gmail.com>
2020-10-12 08:20:07 +0300

    Anonymous functions declared by the inputs and outputs.Not all functions need to be named.

--- src/main/kotlin/function.kt
+++ src/main/kotlin/function.kt
@@ -4,3 +4,12 @@ fun generateMessage(countThreshold: Int): String = if (count > countThreshold) {
 else {
     "There is seats"
 }
+
+//Anonymous functions takes an input of String and return the value an Integer
+val stringLengthFunc: (String) -> Int = {
+    input -> input.length
+}
+
+//Called same as other functions
+val stringLength: Int = stringLengthFunc("Thomas")
+println(stringLength)
