3dbe41e754da603919248edc6ff4b121f3a0cf28
Breens <breensthomas7353@gmail.com>
2020-10-12 06:40:25 +0300

    Variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    //var is used to declare a variable that can change
+    var number: Int = 20
+    number = 25
+
+    //val is used to declare variables that can't be changed
+    val name: Int = "Paul"
 }
